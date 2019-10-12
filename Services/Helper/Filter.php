2055adaeb25131b46c5d87a8e70839b71f3d353c
Volodymyr Volynets <volodymyr.volynets@gmail.com>
2019-10-12 19:46:43 -0400

    use cases from another application.

--- Services/Helper/Filter.php
+++ Services/Helper/Filter.php
@@ -9,6 +9,7 @@ class Filter {
 	const F_SERVSTATUS_CODE = [ 'label_name' => 'Status', 'domain' => 'group_code', 'null' => true, 'null' => true, 'method' => 'multiselect', 'multiple_column' => 1, 'tree' => true, 'options_model' => '\Numbers\Services\Services\DataSource\Statuses::optionsJsonSingle', 'options_options' => ['i18n' => 'skip_sorting']];
 	// service
 	const F_SERVCATEGORY_ID = ['label_name' => 'Service Category', 'domain' => 'category_id', 'null' => true, 'percent' => 50, 'method' => 'multiselect', 'multiple_column' => 1, 'searchable' => true, 'options_model' => '\Numbers\Services\Services\Model\Service\Categories'];
-	const F_SERVICE_ID = ['label_name' => 'Service', 'domain' => 'service_id', 'null' => true, 'percent' => 50, 'method' => 'multiselect', 'multiple_column' => 1, 'tree' => true, 'searchable' => true, 'tree' => true, 'options_model' => '\Numbers\Services\Services\Model\Services::optionsWithCategories'];
+	const F_SERVICE_ID = ['label_name' => 'Service', 'domain' => 'service_id', 'null' => true, 'percent' => 50, 'method' => 'select', 'searchable' => true, 'options_model' => '\Numbers\Services\Services\Model\Services'];
+	const F_SUBSERVICE_NAME = ['label_name' => 'Subservice', 'domain' => 'name', 'null' => true, 'percent' => 50, 'method' => 'select', 'searchable' => true, 'options_model' => '\Numbers\Services\Services\Model\Service\SubServices'];
 	const F_SERVCHANNEL_ID = ['label_name' => 'Channel', 'domain' => 'channel_id', 'null' => true, 'percent' => 50, 'method' => 'multiselect', 'multiple_column' => 1, 'options_model' => '\Numbers\Services\Services\Model\Service\Channels'];
 }
