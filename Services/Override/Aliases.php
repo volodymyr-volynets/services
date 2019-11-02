1e9472bd3fbc2c86f8be07bee9ae28d434326d4e
Volodymyr Volynets <volodymyr.volynets@gmail.com>
2019-11-02 19:20:02 -0400

    Services:	Enhancement \#1276 \(Added alias for service script.\)

--- Services/Override/Aliases.php
+++ Services/Override/Aliases.php
@@ -8,5 +8,10 @@ class Aliases {
 			'no_data_alias_model' => '\Numbers\Services\Services\Model\Service\RedFlags',
 			'no_data_alias_column' => 'ss_servredflag_code'
 		],
+		'servscript_id' => [
+			'no_data_alias_name' => 'Service Script #',
+			'no_data_alias_model' => '\Numbers\Services\Services\Model\ServiceScripts',
+			'no_data_alias_column' => 'ss_servscript_code'
+		]
 	];
 }
